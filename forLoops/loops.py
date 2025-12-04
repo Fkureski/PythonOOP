93596449768c72b28cd1cf18f62cef1f33ae4613
Fkureski <felipekureski@gmail.com>
2025-12-04 15:13:34 -0300

    reminding loops

---
+++ forLoops/loops.py
@@ -0,0 +1,16 @@
+#word = 'FANTASTIC'
+
+#for letter in word:
+#    print(f' {letter}', end='')
+
+
+#Creating a retangle 
+
+lines = 6
+columns = 6 
+symbol = '@'
+
+for l in range(lines):
+    for c in range(columns):
+        print(symbol, end='')
+    print()
