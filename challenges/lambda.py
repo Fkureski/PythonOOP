fdeb3891a4b81f5722ee33dcb5fb8751c5d5aa10
Fkureski <felipekureski@gmail.com>
2026-01-27 14:47:22 -0300

    Lambda challenge. Now I finished the basics

---
+++ challenges/lambda.py
@@ -0,0 +1,21 @@
+'''calc = lambda x: x ** 3
+number = int(input('Enter a number:'))
+print(f'The number {number} cubed is {calc(number)}')'''
+
+'''calc = lambda x, y: x * y
+num1 = int(input('Enter a number:'))
+num2 = int(input('Enter another number:'))
+print(f'The number {num1} multiplied by {num2} is {calc(num1, num2)}')'''
+
+'''number = int(input('Enter a number:'))
+even_or_odd = lambda x: 'even' if x % 2 == 0 else 'odd'
+print(f'The {number} is {even_or_odd(number)}')'''
+
+'''num_list = []
+while len(num_list) < 6:
+    number = int(input('Enter a number:'))
+    num_list.append(number)
+
+squares = list(map(lambda x: x**2, num_list))
+print(squares)'''
+
